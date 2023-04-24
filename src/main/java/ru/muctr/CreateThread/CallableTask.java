526f4c8afb58113bcaaab6a41693b8222de9bbc7
Evgenia <zhenja_a@bk.ru>
2023-04-24 19:13:33 +0300

    Initial commit

---
+++ src/main/java/ru/muctr/CreateThread/CallableTask.java
@@ -0,0 +1,13 @@
+package ru.muctr.CreateThread;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Evgenia Skichko
+ */
+public class CallableTask implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return 1;
+    }
+}
